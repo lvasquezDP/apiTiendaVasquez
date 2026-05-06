afbd33d9a4fee00d756bfc6a6c4b877b16cdca67
lvasquezDP <luisemmanuel.dp@gmail.com>
2026-05-05 20:39:13 -0600

    configuracion de cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 import productRouter from "./router/product.routes";
 import saleRouter from "./router/sale.routes";
 import reportRouter from "./router/report.routes";
 
 const app = express();
-
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 app.use("/product", productRouter);
 app.use("/sale", saleRouter);
