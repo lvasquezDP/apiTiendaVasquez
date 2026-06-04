e6c61763f89d9dcf7bc9d09bc5666bb83ba63825
lvasquezDP <luisemmanuel.dp@gmail.com>
2026-06-03 23:21:23 -0600

    fix save product

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const productSchema = z.object({
   name: z.string(),
   price: z.coerce.number().positive(),
-  realName: z.string().nullable(),
-  realPrice: z.coerce.number().positive().nullable(),
+  realName: z.string().nullable().default(null),
+  realPrice: z.coerce.number().positive().nullable().default(null),
   type: z.enum(["UNIT", "WEIGHT"]),
   stock: z.coerce.number().nonnegative(),
   code: z.string(),
-  supplierId: z.string().nullable()
+  supplierId: z.string().nullable().default(null)
 });
 
 export type Product = z.infer<typeof productSchema>;
